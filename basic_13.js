f1be69a60a521e92dea933c3c5d5495b2d43b7f7
Ln-yd <leslienduong@gmail.com>
2020-07-23 22:40:13 -0700

    first commit

---
+++ basic_13.js
@@ -0,0 +1,14 @@
+// 1)Print 1 - 255
+// Print1To255()
+// Print all the integers from 1 to 255.
+
+function Print1to255() {
+  for (var i = 1; i <= 255; i++) {
+    console.log(i);
+  }
+}
+Print1to255();
+
+// 2. Print Odds 1 - 255
+// PrintOdds1To255()
+// Print all odd integers from 1 to 255.
