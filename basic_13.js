613051d8305c5576e5aee8bfe28f1372e82a3445
Ln-yd <leslienduong@gmail.com>
2020-07-25 15:51:34 -0700

    solved second algo

--- basic_13.js
+++ basic_13.js
@@ -12,3 +12,10 @@ Print1to255();
 // 2. Print Odds 1 - 255
 // PrintOdds1To255()
 // Print all odd integers from 1 to 255.
+
+function PrintOdds1To255() {
+  for (var i = 1; i <= 255; i += 2) {
+    console.log(i);
+  }
+}
+PrintOdds1To255();
