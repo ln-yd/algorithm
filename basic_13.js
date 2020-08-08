9c86869b34683ca8e3aa99984447a567a19be2ef
Ln-yd <leslienduong@gmail.com>
2020-08-07 22:34:09 -0700

    solve algo # 11

--- basic_13.js
+++ basic_13.js
@@ -149,3 +149,26 @@ function ZeroOutArrayNegativeVals(arr) {
 }
 
 console.log(ZeroOutArrayNegativeVals([-1, 4, 6, -3, 5]));
+
+// 11. Max, Min, Average
+// PrintMaxMinAverageArrayVals(arr)
+// Given an array, print the max, min and average values for that array.
+
+function PrintMaxMinAverageArrayVals(arr) {
+  var Max = arr[0];
+  var min = arr[0];
+  var sum = 0;
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > Max) {
+      Max = arr[i];
+    }
+    sum = sum + arr[i];
+  }
+  avg = sum / arr.length;
+  console.log(Max, min, avg);
+}
+
+PrintMaxMinAverageArrayVals([2, 4, 6, 2, 5, 64]);
